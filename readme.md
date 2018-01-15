a412fd6d9a91cf85d6c8cedfcf1ddb36898e982a
Ann Daramola <ann.daramola@gmail.com>
2018-01-15 15:01:31 -0800

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,13 @@
+S W O O S H 
+
+A simple, slightly flirty, tool for adding all them hosts you be ssh'ing into. It's called `swoosh`, a term I got from Scott Hanselman a while ago.
+
+I know there's the ~/.ssh/config file but I made this for fun. Feel free to do with it what you please.
+
+Installation
+* Clone this repo to your home directory.
+* Update the path to the .sites file at the top of swoosh.sh
+* Add the script to your path: `export PATH="$PATH:[PATH_TO_SWOOSH]"`
+* Happy swooshing!
+
+
---
+++ swoosh.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+# change this to the paths for the .sites file and the .swoosh script
+alias srcsites="source ~/.sites;"
+alias src="source ~/.swoosh.sh"
+
+newswoosh() {
+    if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+        newsite="sites["
+        
+        echo "Hey love! Let's add a new ssh host to the database".
+        echo -e "Please provide a ${MAGEN}sitename${NCN}."
+        read sitename
+        newsite="$newsite$sitename]="
+        
+        echo -e "Aiite, now lemme get the ${MAGEN}username${NCN} for this site."
+        read username
+        newsite="$newsite$username@"
+
+        echo -e "Thanks, boo. And what's the host?"
+        read hostname
+        newsite="$newsite$hostname"
+        echo "$newsite" >> ~/.sites
+        srcsites
+        return 
+    else
+        echo "sites[$1]=$2@$3" >> ~/.sites
+        srcsites
+        return
+    fi
+}
+
+swoosh() {
+    if [ -z "$1" ]; then
+        srcsites
+        echo "Sorry, babe, I need a site to swoosh. Choose one:"
+        for K in "${!sites[@]}"; do
+            echo -e "Write ${GREEN}${BOLD}swoosh $K ${NCN}${NCN}for ${MAGEN}${BOLD}${sites[$K]}${NCN}${NCN}";
+        done
+    else
+        echo "Swoooooooooooooooooooooooooooooooooosh"
+        ssh ${sites[$1]}
+        return
+    fi
+}
